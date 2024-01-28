74a0c0e98eab31fdb78fa0c1281de7746e563667
Sakshi verma <103350285+volimte29@users.noreply.github.com>
2024-01-28 15:31:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Board } from './components/Board';
+
+function App() {
+  return (
+    <div className='App'>
+      <Board></Board>
+    </div>
+   
+   
+  );
+}
+
+export default App;
